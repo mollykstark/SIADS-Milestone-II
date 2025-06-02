4ee018092dba78110aa0943a7918ba8bcd943cd0
Molly Stark <mollysta@umich.edu>
2025-06-02 14:25:13 -0700

    some code cleanup

--- src/notebooks/met_cleaning.py
+++ src/notebooks/met_cleaning.py
@@ -1,8 +1,11 @@
 """
 Python Script to create a column labeling the country of origin for MET artworks.
 """
+import warnings
 from country_name import determine_met_country_names
 import pandas as pd
+
+warnings.filterwarnings('ignore')
 
 def clean_met_data() -> pd.DataFrame:
     """
@@ -12,7 +15,7 @@ def clean_met_data() -> pd.DataFrame:
         None
 
     Returns:
-        Pandas DataFrame: A Pandas dataframe containing the 
+        Pandas DataFrame:A Pandas dataframe containing the 
         original dataframe with the additional country column.
     """
 
@@ -20,35 +23,53 @@ def clean_met_data() -> pd.DataFrame:
     met = pd.read_csv('https://media.githubusercontent.com/media/metmuseum'
                       '/openaccess/refs/heads/master/MetObjects.csv')
 
-    met = met.rename(columns={'Object ID':'object_id','Object Number': 'object_number', 'Is Highlight': 'is_highlight', 'Is Timeline Work': 'is_timeline_work', 'Is Public Domain': 'is_public_domain',
-        'Gallery Number':'gallery_number', 'Department':'department', 'AccessionYear':'accession_year', 'Object Name':'object_name', 'Title':'title',
-        'Culture':'culture', 'Period': 'period', 'Dynasty': 'dynasty', 'Reign': 'reign', 'Portfolio': 'portfolio', 'Constituent ID': 'constituent_id',
-        'Artist Role':'artist_role', 'Artist Prefix':'artist_prefix', 'Artist Display Name':'artist_display_name',
-        'Artist Display Bio': 'artist_display_bio', 'Artist Suffix':'artist_suffix', 'Artist Alpha Sort':'artist_alpha_sort',
-        'Artist Nationality':'artist_nationality', 'Artist Begin Date':'artist_begin_date', 'Artist End Date':'artist_end_date',
-        'Artist Gender':'artist_gender', 'Artist ULAN URL':'artist_ulan_url', 'Artist Wikidata URL':'artist_wikidata_url',
-        'Object Date':'object_date', 'Object Begin Date':'object_begin_date', 'Object End Date':'object_end_date', 'Medium':'medium',
-        'Dimensions':'dimensions', 'Credit Line':'credit_line', 'Geography Type':'geography_type', 'City':'city', 'State':'state',
-        'County':'county', 'Country':'country', 'Region':'region', 'Subregion':'subregion', 'Locale':'locale', 'Locus':'locus',
-        'Excavation':'excavation', 'River':'river', 'Classification':'classification', 'Rights and Reproduction':'rights_and_reproduction',
-        'Link Resource':'link_resource', 'Object Wikidata URL':'object_wikidata_url', 'Metadata Date':'metadata_date', 'Repository':'repository',
-        'Tags':'tags', 'Tags AAT URL':'tags_aat_url', 'Tags Wikidata URL':'tags_wikidata_url'})
+    met = met.rename(columns={'Object ID':'object_id','Object Number':'object_number',
+                              'Is Highlight':'is_highlight', 'Is Timeline Work':'is_timeline_work', 
+                              'Is Public Domain':'is_public_domain','Gallery Number':
+                              'gallery_number', 'Department':'department', 'AccessionYear':
+                              'accession_year', 'Object Name':'object_name', 'Title':'title',
+                              'Culture':'culture', 'Period':'period', 'Dynasty':'dynasty',
+                              'Reign':'reign', 'Portfolio':'portfolio', 'Constituent ID':
+                              'constituent_id','Artist Role':'artist_role', 'Artist Prefix':
+                              'artist_prefix', 'Artist Display Name':'artist_display_name',
+                              'Artist Display Bio':'artist_display_bio', 'Artist Suffix':
+                              'artist_suffix', 'Artist Alpha Sort':'artist_alpha_sort',
+                              'Artist Nationality':'artist_nationality', 'Artist Begin Date':
+                              'artist_begin_date', 'Artist End Date':'artist_end_date',
+                              'Artist Gender':'artist_gender', 'Artist ULAN URL':'artist_ulan_url',
+                              'Artist Wikidata URL':'artist_wikidata_url','Object Date':
+                              'object_date', 'Object Begin Date':'object_begin_date',
+                              'Object End Date':'object_end_date', 'Medium':'medium',
+                              'Dimensions':'dimensions', 'Credit Line':'credit_line',
+                              'Geography Type':'geography_type', 'City':'city', 'State':
+                              'state','County':'county', 'Country':'country', 'Region':
+                              'region', 'Subregion':'subregion', 'Locale':'locale', 'Locus':
+                              'locus','Excavation':'excavation', 'River':'river',
+                              'Classification':'classification', 'Rights and Reproduction':
+                              'rights_and_reproduction','Link Resource':'link_resource',
+                              'Object Wikidata URL':'object_wikidata_url', 'Metadata Date':
+                              'metadata_date', 'Repository':'repository','Tags':'tags',
+                              'Tags AAT URL':'tags_aat_url', 'Tags Wikidata URL':
+                              'tags_wikidata_url'})
     met.set_index('object_id', inplace=True)
 
     met = determine_met_country_names(met, "../data/helper/countries.csv")
 
     met['is_significant'] = met['is_timeline_work'] | met['is_highlight']
 
-    met = met.replace({'object_date':{"–":"-", " ":"", "ca.":"", "before":"","after":"","edition":"", "()":"","probably":"", "Probably":"","early":"", "late":"", r'\?':"",
-                                    "20thcentury":"1900", "19thcentury":"1800", "18thcentury":"1700", "17thcentury":"1600", "16thcentury":"1500",
-                                    "15thcentury":"1400", "14thcentury":"1300", "13thcentury":"1200", "12thcentury":"1100", "11thcentury":"1000",
-                                    "10thcentury":"1400", "9thcentury":"1300", "8thcentury":"1200", "7thcentury":"1100", "6thcentury":"1000",
-                                    "5thcentury":"400", "4thcentury":"300", "3rdcentury":"200", "2ndcentury":"100", "1stcentury":"0000",
-                                    "by":"", "or":"-",
-                                    
-                                    }}, regex=True)
+    met = met.replace({'object_date':{"–":"-", " ":"", "ca.":"", "before":"","after":"","edition":
+                                      "","()":"","probably":"", "Probably":"","early":"", "late":"",
+                                        r'\?':"","20thcentury":"1900", "19thcentury":"1800",
+                                        "18thcentury":"1700", "17thcentury":"1600", "16thcentury":
+                                        "1500","15thcentury":"1400", "14thcentury":"1300", 
+                                        "13thcentury":"1200", "12thcentury":"1100", "11thcentury":
+                                        "1000","10thcentury":"1400", "9thcentury":"1300",
+                                        "8thcentury":"1200", "7thcentury":"1100", "6thcentury":
+                                        "1000","5thcentury":"400", "4thcentury":"300", "3rdcentury":
+                                        "200", "2ndcentury":"100", "1stcentury":"0000","by":"",
+                                        "or":"-",}}, regex=True)
 
-    met['extracted_date'] = met['object_date'].str.findall(r'\d{4}').str[0]
+    met['age'] = 2025 - pd.to_numeric(met['object_begin_date'])
     met['access_year'] = met['accession_year'].astype(str).str.findall(r'\d{4}').str[0]
 
     met['artist_gender'] = met['artist_gender'].replace(False, "0")
@@ -62,6 +83,6 @@ def clean_met_data() -> pd.DataFrame:
             'artist_display_name', 
             'artist_nationality', 
             'artist_gender', 'mapped_country',
-            'extracted_date',  'medium', 'tags']]
+            'age',  'medium', 'tags']]
 
     return met
--- src/notebooks/preprocessing.py
+++ src/notebooks/preprocessing.py
@@ -1,32 +1,24 @@
 """
 Python Script to create a column labeling the country of origin for MET artworks.
 """
-from met_cleaning import clean_met_data
 from sklearn.model_selection import train_test_split
 from sklearn.pipeline import Pipeline
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.impute import SimpleImputer
 from sklearn.compose import ColumnTransformer
 from sklearn.preprocessing import OneHotEncoder
-from sklearn.metrics import mean_absolute_error
-from imblearn.ensemble import BalancedRandomForestClassifier
 from imblearn.over_sampling import SMOTE
 
-df = clean_met_data()
-
-X = df.drop('is_significant', axis=1)
-y = df['is_significant']
-
-numeric_features = ['accession_year', 'extracted_date']
+numeric_features = ['access_year', 'age']
 numeric_transformer = Pipeline(steps=[("imputer",
-                                       SimpleImputer(strategy="constant", fill_value=0)),])
+                                    SimpleImputer(strategy="constant", fill_value=0)),])
 
 categorical_features = ['department','object_name','title','culture','portfolio',
                         'artist_display_name','artist_nationality','mapped_country',
                         'medium', 'artist_gender']
 categorical_transformer = Pipeline(steps=[('imputer', SimpleImputer(strategy="constant",
                                                                     fill_value="missing")),
-                                      ('onehot', OneHotEncoder(handle_unknown="ignore"))])
+                                    ('onehot', OneHotEncoder(handle_unknown="ignore"))])
 
 tag_features = ['tags']
 tag_transformer = Pipeline(steps=[('tfidf', TfidfVectorizer())])
@@ -39,27 +31,19 @@ preprocessor = ColumnTransformer(
     ]
 )
 
-pipe = Pipeline(steps=[('preprocessor', preprocessor),
-                       ('classifier', BalancedRandomForestClassifier())
-                       ])
-
 smote = SMOTE(random_state=42)
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.8,random_state=42)
+def supervised_preprocessing(df):
+    X = df.drop('is_significant', axis=1)
+    y = df['is_significant']
 
-X_train_resampled, y_train_resampled = smote.fit_resample(X_train, y_train)
+    X = preprocessor.fit_transform(X)
 
-pipe.fit(X_train_resampled, y_train_resampled)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.8,random_state=42)
+    X_train_resampled, y_train_resampled = smote.fit_resample(X_train, y_train)
+    return X_train, X_test, y_train, y_test, X_train_resampled, y_train_resampled
 
-metrics = {
-    "train_data":{
-        "score": pipe.score(X_train, y_train),
-        "mae": mean_absolute_error(y_train, pipe.predict(X_train))
-    },
-    "test_data":{
-        "score": pipe.score(X_test, y_test),
-        "mae": mean_absolute_error(y_test, pipe.predict(X_test))
-    }
-}
-
-print(metrics)
+def unsupervised_preprocessing(df):
+    X = df.drop('is_significant', axis=1)
+    X = preprocessor.fit_transform(X)
+    return X
